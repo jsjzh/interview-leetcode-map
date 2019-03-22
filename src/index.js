4303f2054143c78b0265dbbfc17de6d82197ea71
jsjzh <kimimi_king@163.com>
2019-03-22 22:29:51 +0800

    上传一个手写_promise_的代码，有空了看看

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './index.css'
 
 import './init'
 // ------------- leetcode -------------
-import 'leetcode'
+// import 'leetcode'
 // ------------- interview -------------
 // import 'interview'
+import 'interview/promise'
