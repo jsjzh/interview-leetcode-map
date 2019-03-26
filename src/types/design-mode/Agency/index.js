7b73190c1525b54f43b85345c76b47f93e4785f1
jsjzh <kimimi_king@163.com>
2019-03-26 09:05:27 +0800

    添加一个设计模式中的代理模式

--- src/types/design-mode/Agency/index.js
+++ src/types/design-mode/Agency/index.js
@@ -3,35 +3,33 @@
  * @Email: kimimi_king@163.com
  * @LastEditors: jsjzh
  * @Date: 2019-03-25 22:17:07
- * @LastEditTime: 2019-03-25 22:26:47
+ * @LastEditTime: 2019-03-26 09:02:20
  * @Description: 代理模式
  * 为一个对象提供一个代用品或占位符，以便控制对它的访问
  *
  * 常用的虚拟代理形式，某一个花销很大的操作，可以通过虚拟代理的方式
  * 延迟需要他的时候采取创建（如：使用虚拟代理实现图片懒加载）
- * 先通过一张 loading 的图占位，通过异步的方式加载图片，等图片加载好了
+ * 先通过一张 loading 的图占位（小图，加载快），通过异步的方式加载图片，等图片加载好了
  * 再把完成的图片加载到 img 标签里面
  */
 
-let imgFunc = (function() {
-  let imgNode = document.createElement('img')
-  document.body.appendChild(imgNode)
+export const setImg = (function() {
   return {
-    setSrc(src) {
-      imgNode.src = src
+    setSrc(targetEl, src) {
+      targetEl.src = src
     }
   }
 })()
 
-export const Agency = (function() {
-  let img = new Image()
-  img.onload = function() {
-    imgFunc.setSrc(this.src)
-  }
+export const setImgAgency = (function() {
   return {
-    setSrc(src) {
-      imgFunc.setSrc('http://www.sucaijishi.com/uploadfile/2015/0210/20150210104951657.gif')
+    setSrc(targetEl, src) {
+      let img = new Image()
       img.src = src
+      img.onload = function() {
+        setImg.setSrc(targetEl, src)
+      }
+      targetEl.src = 'http://www.sucaijishi.com/uploadfile/2015/0210/20150210104951657.gif'
     }
   }
 })()
--- src/types/design-mode/index.js
+++ src/types/design-mode/index.js
@@ -1,7 +1,7 @@
 import { ProxySingleton } from './Singleton'
 import { strategy } from './Strategy'
-import { Agency } from './Agency'
+import { setImg, setImgAgency } from './Agency'
 
 console.log(new ProxySingleton('foo') === new ProxySingleton('bar'))
 console.log(strategy('add', 1)), console.log(strategy('minus', 1))
-Agency.setSrc('http://lensbuyersguide.com/gallery/219/2/23_iso100_14mm.jpg')
+setImgAgency.setSrc(document.getElementById('agencyImg'), 'http://lensbuyersguide.com/gallery/219/2/23_iso100_14mm.jpg')
