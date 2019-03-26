36b72d62a93a03cfd103ca697f6b6218eb62afc3
jsjzh <kimimi_king@163.com>
2019-03-26 12:41:21 +0800

    edit

--- src/types/design-mode/index.js
+++ src/types/design-mode/index.js
@@ -1,6 +1,12 @@
+// 单例模式
 // import './Singleton'
+// 策略模式
 // import './Strategy'
+// 代理模式
 // import './Agency'
+// 中介者模式
 // import './Intermediary'
-import './Decorator'
-// import './Factory'
+// 装饰者模式
+// import './Decorator'
+// 工厂模式
+import './Factory'
