78a4d83bb3060fab1b529620d3c05d3b42a182b3
jsjzh <kimimi_king@163.com>
2019-05-20 14:02:52 +0800

    添加一道-leetcode-题目

--- src/types/leetcode/index.js
+++ src/types/leetcode/index.js
@@ -3,7 +3,7 @@
  * @Email: kimimi_king@163.com
  * @LastEditors: jsjzh
  * @Date: 2019-03-08 09:45:09
- * @LastEditTime: 2019-05-20 11:48:29
+ * @LastEditTime: 2019-05-20 13:32:17
  * @Description
  *  果然每天的生活都需要点算法题调剂调剂，每天都是重复的业务代码太无趣了，我渴望一点需要动脑子的东西，遂就有了这个小项目
  *  写上来的代码都是可以通过 leedcode 的测试的，只不过嘛，用时和内存消耗就没有那么完美了，但我会对不满意的题目重写一遍，开拓新的思路，撒花
@@ -873,7 +873,7 @@ let getRow = function(rowIndex) {}
 // console.log(getRow(10))
 
 /**
- * TODO
+ * CLEAR
  * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
  * 如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
  * 注意你不能在买入股票前卖出股票。
@@ -889,7 +889,7 @@ let getRow = function(rowIndex) {}
  * @param {Number[]} prices
  * @return {Number}
  */
-let maxProfit = function(prices) {
+let maxProfitOne = function(prices) {
   let max = 0
   for (let i = prices.length - 1; i >= 0; i--) {
     for (let j = i - 1; j >= 0; j--) {
@@ -898,4 +898,33 @@ let maxProfit = function(prices) {
   }
   return max
 }
-// console.log(maxProfit([7, 6, 4, 3, 1]))
+// console.log(maxProfitOne([7, 6, 4, 3, 1]))
+
+/**
+ * TODO
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ * 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ *
+ * 输入 [7,1,5,3,6,4]
+ * 输出 7
+ * 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+ * 随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6-3 = 3 。
+ *
+ * 输入 [1,2,3,4,5]
+ * 输出 4
+ * 在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+ * 注意你不能在第 1 天和第 2 天接连购买股票，之后再将它们卖出。
+ * 因为这样属于同时参与了多笔交易，你必须在再次购买前出售掉之前的股票。
+ *
+ * 输入 [7,6,4,3,1]
+ * 输出 0
+ * 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ * @param {Number[]} prices
+ * @return {Number}
+ */
+let maxProfitTwo = function(prices) {
+  return prices
+}
+
+console.log(maxProfitTwo([7, 1, 5, 3, 6, 4]))
